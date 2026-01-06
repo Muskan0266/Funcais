b90375f1b74db902780e131482a02b7ca45f9c6e
Muskan0266 <25muskaaan@gmail.com>
2026-01-06 20:15:58 +0530

    server.cjs

--- Backend/src/server.cjs
+++ Backend/src/server.cjs
@@ -108,12 +108,11 @@ app.post("/login", async (req, res) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" });
 
         res.cookie("token", token, {
-
             httpOnly: true,
-            secure: true,
-            sameSite: "none",
+            secure: process.env.NODE_ENV === "production",   // ✅ false locally
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // ✅ lax locally
             path: "/",
-            maxAge: 7 * 24 * 60 * 60 * 1000
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
         res.json({
             message: "Login successful",
