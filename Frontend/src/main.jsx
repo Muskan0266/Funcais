a26decce25cbfde9fc218110fdbe6abd80abf81d
Muskan0266 <25muskaaan@gmail.com>
2026-01-06 20:49:00 +0530

    updated

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { UserProvider } from "./components/UserContext";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
-  </React.StrictMode>
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
