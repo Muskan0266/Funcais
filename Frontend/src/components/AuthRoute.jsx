5d49383450412e2e70c322b3c23041403e4cb853
Muskan0266 <25muskaaan@gmail.com>
2026-01-07 19:32:05 +0530

    u

--- Frontend/src/components/AuthRoute.jsx
+++ Frontend/src/components/AuthRoute.jsx
@@ -1,17 +1,40 @@
-// components/IsAuthN.jsx
 import { Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
 
 const AuthRoute = ({ element, authType }) => {
-    const isAuth = localStorage.getItem("token");
+    const { user, loading } = useContext(UserContext);
 
+    if (loading) return <div className="text-center mt-20">Loading...</div>;
+
+    // ---------- PROTECTED ROUTES ----------
     if (authType === "protected") {
-        // For pages like /main, /profile, /challenges
-        return isAuth ? element : <Navigate to="/login" replace />;
+        // not logged in
+        if (!user) return <Navigate to="/" replace />;
+
+        const isSetupComplete = Boolean(user.purpose) && Boolean(user.level);
+
+        // user logged in but NOT finished onboarding
+        if (!isSetupComplete) {
+            if (!user.purpose) return <Navigate to="/purpose" replace />;
+
+        }
+
+        // setup complete -> allow normal access
+        return <Navigate to="/main" replace />
     }
 
+    // ---------- PUBLIC ROUTES ----------
     if (authType === "public") {
-        // For pages like /login, /signup, /landing
-        return isAuth ? <Navigate to="/main" replace /> : element;
+        // not logged in → can access
+        if (!user) return element;
+
+        // logged in
+        const isSetupComplete = Boolean(user.purpose) && Boolean(user.level);
+
+        return isSetupComplete
+            ? <Navigate to="/main" replace />
+            : <Navigate to="/purpose" replace />;
     }
 
     return element;
